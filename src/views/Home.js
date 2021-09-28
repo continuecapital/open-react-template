55182a448a4a61bd6a5cc79bb179151e30f4c74a
qiuchenghui <243852877@qq.com>
2021-09-29 02:17:14 +0800

    feat: remove cta.

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,7 @@ import Hero from '../components/sections/Hero'
 import FeaturesTiles from '../components/sections/FeaturesTiles'
 import FeaturesSplit from '../components/sections/FeaturesSplit'
 import Testimonial from '../components/sections/Testimonial'
-import Cta from '../components/sections/Cta'
+// import Cta from '../components/sections/Cta'
 import Portfolio from '../components/sections/Portfolio'
 import Contact from '../components/sections/Contact'
 
@@ -17,7 +17,7 @@ const Home = () => {
       <Testimonial id="team" topDivider />
       <Portfolio id="portfolio" invertMobile topDivider />
       <Contact id="contact" invertMobile topDivider />
-      <Cta split />
+      {/* <Cta split /> */}
     </>
   )
 }
